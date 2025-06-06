3ebda3f8815eac914481a22a2248cc24788fbd25
H. Watanabe <kaityo@users.sourceforge.jp>
2025-06-06 14:09:21 +0900

    :memo: updates

--- prepare.py
+++ prepare.py
@@ -1,7 +1,30 @@
 import subprocess
 
 
-def save_jobfile(temp_array):
+def save_serialjobfile(temp_array):
+    str = ""
+    for temperature in temp_array:
+        prefix = f"T{int(temperature*100):03d}"
+        str += f"srun lammps < {prefix}/{prefix}.input\n"
+    content = f"""\
+#!/bin/sh
+
+#SBATCH -p i8cpu
+#SBATCH -N 1
+
+source /home/issp/materiapps/intel/lammps/lammpsvars.sh
+SECONDS=0
+
+{str}
+"""
+    content += 'echo "Elapsed time: ${SECONDS} seconds"\n'
+    filename = "data/job1.sh"
+    with open("data/job1.sh", "w") as f:
+        f.write(content)
+    print(f"Generated {filename}")
+
+
+def save_bulkjobfile(temp_array):
     str = ""
     for temperature in temp_array:
         prefix = f"T{int(temperature*100):03d}"
@@ -30,11 +53,12 @@ wait
 def main():
     temp_array = [0.9, 0.92, 0.94, 0.96, 0.98, 1.00, 1.02, 1.04]
     density = 0.9
-    save_jobfile(temp_array)
+    save_serialjobfile(temp_array)
+    save_bulkjobfile(temp_array)
     for t in temp_array:
         cmd = ["python3", "generate_config.py", "-d", str(density), "-t", str(t)]
         print(f"Running: {' '.join(cmd)}")
-        subprocess.run(cmd, check=True)
+        # subprocess.run(cmd, check=True)
 
 
 if __name__ == "__main__":
